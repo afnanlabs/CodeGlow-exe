49f724330fe964181e2ff884aae9c98ccaa12ba4
Afnan Khan <khanafnan1108@gmail.com>
2026-06-06 01:18:37 +0530

    feat(storage): add workspace state persistence manager

---
+++ codeglow/src/storageManager.ts
@@ -0,0 +1,16 @@
+import * as vscode from 'vscode';
+import { HighlightMap } from './types';
+
+const STORAGE_KEY = 'codeglow.highlights';
+
+export class StorageManager {
+	constructor(private readonly context: vscode.ExtensionContext) {}
+
+	load(): HighlightMap {
+		return this.context.workspaceState.get<HighlightMap>(STORAGE_KEY, {});
+	}
+
+	async save(map: HighlightMap): Promise<void> {
+		await this.context.workspaceState.update(STORAGE_KEY, map);
+	}
+}
